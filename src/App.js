766d3791bc49c7ceec3c1daf0e350dcafa626954
Eric Hendrix <agentmrbig@gmail.com>
2019-05-28 21:17:27 -0400

    setup styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const AppStyle = styled.div`
+  text-align: center;
+`
+
+const AppLogo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+`
+
+const AppHeader = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+const AppIntro = styled.p`
+  font-size: large;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <AppStyle>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        </AppHeader>
+        <AppIntro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </AppIntro>
+      </AppStyle>
     );
   }
 }
